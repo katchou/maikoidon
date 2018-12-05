4d2d7f7786853b2826d769c8c5a1c21357ad955e
Jean-François Jung <jean-francois.jung@capgemini.com>
2018-12-05 12:07:05 +0100

    maintenant, on va faire des templates

--- src/app/prestations/prestations-routing.module.ts
+++ src/app/prestations/prestations-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageAddPrestationComponent } from './pages/page-add-prestation/page-add-prestation.component';
 import { PagesPrestationsComponent } from './pages/pages-prestations/pages-prestations.component';
 
 const appRoutes: Routes = [
   { path: '', component: PagesPrestationsComponent },
-  { path: 'add', component: PagesPrestationsComponent },
+  { path: 'add', component: PageAddPrestationComponent },
 ];
 
 
